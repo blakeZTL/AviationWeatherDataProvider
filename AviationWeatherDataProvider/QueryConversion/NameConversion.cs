c93755aea917106979494bf52d9c11dbfdc6b322
Blake Bradford <b@blrfam.win>
2025-02-23 11:09:25 -0500

    Add NameConversion class for query transformation
    A new file `NameConversion.cs` has been added to the project `AviationWeatherDataProvider.csproj`. This file introduces a static class `NameConversion` within the `AviationWeatherDataProvider.QueryConversion` namespace. The class includes two primary methods:
    
    - `TransformQueryExpression`: Processes a `QueryExpression` object, logs the start and end times, handles conditions to transform the query, and returns a query parameter along with a list of unhandled expressions.
    - `ProcessUnhandeledExpressions`: Processes a collection of `Entity` objects against a list of unhandled `ConditionExpression` objects, filters the entities based on various condition operators, logs the processing steps, and returns the filtered collection of entities.
    
    The class makes extensive use of tracing to log processing steps and data states. The file includes necessary `using` directives for relevant namespaces.

---
+++ AviationWeatherDataProvider/QueryConversion/NameConversion.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace AviationWeatherDataProvider.QueryConversion
+{
+    public static class NameConversion
+    {
+        public static string TransformQueryExpression(
+            QueryExpression query,
+            ITracingService tracer,
+            out List<ConditionExpression> unhandeledExpressions
+        )
+        {
+            tracer.Trace(nameof(TransformQueryExpression) + $" Start: {DateTime.Now}");
+            unhandeledExpressions = new List<ConditionExpression>();
+
+            string queryParameter = null;
+            if (
+                query == null
+                || (!query.Criteria.Conditions.Any() && !query.Criteria.Filters.Any())
+            )
+            {
+                tracer.Trace("Query is empty.");
+                tracer.Trace(nameof(TransformQueryExpression) + $" End: {DateTime.Now}");
+                return queryParameter;
+            }
+
+            ProcessConditions(query.Criteria.Conditions.ToList(), unhandeledExpressions);
+            var filterConditions = query.Criteria.Filters.SelectMany(f => f.Conditions).ToList();
+            ProcessConditions(filterConditions, unhandeledExpressions);
+
+            void ProcessConditions(
+                List<ConditionExpression> conditions,
+                List<ConditionExpression> expressions
+            )
+            {
+                foreach (var condition in conditions)
+                {
+                    if (condition.AttributeName != "awx_name")
+                    {
+                        tracer.Trace($"Attribute {condition.AttributeName} is not supported.");
+                        continue;
+                    }
+
+                    if (condition.Operator == ConditionOperator.Equal)
+                    {
+                        queryParameter = condition.Values[0] as string;
+                        break;
+                    }
+                    else if (condition.Operator == ConditionOperator.In)
+                    {
+                        queryParameter = string.Join(
+                            ",",
+                            condition.Values.Select(v => v as string)
+                        );
+                        break;
+                    }
+                    else
+                    {
+                        tracer.Trace($"Operator {condition.Operator} is unhandeled.");
+                        expressions.Add(condition);
+                    }
+                }
+            }
+
+            tracer.Trace(nameof(TransformQueryExpression) + $" End: {DateTime.Now}");
+            return queryParameter;
+        }
+
+        public static EntityCollection ProcessUnhandeledExpressions(
+            EntityCollection metars,
+            List<ConditionExpression> unhandeledExpressions,
+            ITracingService tracer
+        )
+        {
+            tracer.Trace(nameof(ProcessUnhandeledExpressions) + $" Start: {DateTime.Now}");
+            tracer.Trace($"Unhandeled expressions count: {unhandeledExpressions.Count}");
+            tracer.Trace($"Metars count: {metars.Entities.Count}");
+
+            if (unhandeledExpressions == null || !unhandeledExpressions.Any())
+            {
+                return metars;
+            }
+
+            foreach (var exp in unhandeledExpressions)
+            {
+                tracer.Trace(
+                    $"Processing unhandeled expression: {exp.AttributeName} {exp.Operator} {string.Join(",", exp.Values.Select(v => v as string))}"
+                );
+                var debugMetars = metars.Entities.Where(m => (string)m["awx_name"] == "KATL");
+                tracer.Trace($"debugMetars: {debugMetars.Count()}");
+                switch (exp.Operator)
+                {
+                    case ConditionOperator.NotEqual:
+                        var filteredEntities = metars.Entities.Where(m =>
+                            (string)m["awx_name"] != exp.Values[0] as string
+                        );
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(filteredEntities);
+                        break;
+                    case ConditionOperator.Like:
+                        var likeValue = exp.Values[0] as string;
+                        tracer.Trace($"Like value: {likeValue}");
+                        var regexPattern = "^" + Regex.Escape(likeValue).Replace("%", ".*") + "$";
+                        var likeEntities = metars
+                            .Entities.Where(m => Regex.IsMatch((string)m["awx_name"], regexPattern))
+                            .ToList();
+                        tracer.Trace("Entities after Like filter:");
+                        foreach (var entity in likeEntities)
+                        {
+                            tracer.Trace($"awx_name: {(string)entity["awx_name"]}");
+                        }
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(likeEntities);
+                        break;
+                    case ConditionOperator.NotLike:
+                        var notLikeValue = exp.Values[0] as string;
+                        var notLikePattern =
+                            "^" + Regex.Escape(notLikeValue).Replace("%", ".*") + "$";
+                        var notLikeEntities = metars
+                            .Entities.Where(m =>
+                                !Regex.IsMatch((string)m["awx_name"], notLikePattern)
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(notLikeEntities);
+                        break;
+                    case ConditionOperator.NotIn:
+                        var notInEntities = metars
+                            .Entities.Where(m =>
+                                !exp.Values.Select(v => v as string).Contains((string)m["awx_name"])
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(notInEntities);
+                        break;
+                    case ConditionOperator.Null:
+                        var nullEntities = metars
+                            .Entities.Where(m =>
+                                m.Attributes.ContainsKey("awx_name") && m["awx_name"] == null
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(nullEntities);
+                        break;
+                    case ConditionOperator.NotNull:
+                        var notNullEntities = metars
+                            .Entities.Where(m =>
+                                m.Attributes.ContainsKey("awx_name") && m["awx_name"] != null
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(notNullEntities);
+                        break;
+                    case ConditionOperator.Contains:
+                        var containsValue = exp.Values[0] as string;
+                        var containsPattern = ".*" + Regex.Escape(containsValue) + ".*";
+                        var containsEntities = metars
+                            .Entities.Where(m =>
+                                Regex.IsMatch((string)m["awx_name"], containsPattern)
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(containsEntities);
+                        break;
+                    case ConditionOperator.DoesNotContain:
+                        var doesNotContainValue = exp.Values[0] as string;
+                        var doesNotContainPattern = ".*" + Regex.Escape(doesNotContainValue) + ".*";
+                        var doesNotContainEntities = metars
+                            .Entities.Where(m =>
+                                !Regex.IsMatch((string)m["awx_name"], doesNotContainPattern)
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(doesNotContainEntities);
+                        break;
+                    case ConditionOperator.BeginsWith:
+                        var beginsWithValue = exp.Values[0] as string;
+                        var beginsWithPattern = "^" + Regex.Escape(beginsWithValue);
+                        var beginsWithEntities = metars
+                            .Entities.Where(m =>
+                                Regex.IsMatch((string)m["awx_name"], beginsWithPattern)
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(beginsWithEntities);
+                        break;
+                    case ConditionOperator.DoesNotBeginWith:
+                        var doesNotBeginWithValue = exp.Values[0] as string;
+                        var doesNotBeginWithPattern = "^" + Regex.Escape(doesNotBeginWithValue);
+                        var doesNotBeginWithEntities = metars
+                            .Entities.Where(m =>
+                                !Regex.IsMatch((string)m["awx_name"], doesNotBeginWithPattern)
+                            )
+                            .ToList();
+                        metars.Entities.Clear();
+                        metars.Entities.AddRange(doesNotBeginWithEntities);
+                        break;
+                    default:
+                        tracer.Trace($"Operator {exp.Operator} is not supported.");
+                        break;
+                }
+            }
+
+            tracer.Trace(
+                $"Metars count after processing unhandeled expressions: {metars.Entities.Count}"
+            );
+            tracer.Trace(nameof(ProcessUnhandeledExpressions) + $" End: {DateTime.Now}");
+            return metars;
+        }
+    }
+}
