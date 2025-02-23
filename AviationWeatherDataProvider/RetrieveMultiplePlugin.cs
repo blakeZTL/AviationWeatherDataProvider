1be4ca3baf41f4188df28108965c45b7f2efa2ad
Blake Bradford <b@blrfam.win>
2025-02-23 11:09:39 -0500

    Refactor query handling and add error handling
    Refactored `ExecuteCdsPlugin` to use the new `NameConversion` class for query transformations and processing unhandled expressions. Removed the `ParseMetarNameQuery` method and its associated logic. Updated `using` directives. Added error handling in `GetMetarsByStationString` for cases with too many results.

--- AviationWeatherDataProvider/RetrieveMultiplePlugin.cs
+++ AviationWeatherDataProvider/RetrieveMultiplePlugin.cs
@@ -3,18 +3,14 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Xml.Serialization;
+using AviationWeatherDataProvider.QueryConversion;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Metadata;
 using Microsoft.Xrm.Sdk.Query;
 
 namespace AviationWeatherDataProvider
 {
     public class RetrieveMultiplePlugin : PluginBase
     {
-        //static string _stationString = null;
-        //static string[] _antiStationString = { };
-        //static string _stationIdBeginsWith = null;
-
         public RetrieveMultiplePlugin()
             : base(typeof(RetrieveMultiplePlugin)) { }
 
@@ -23,17 +19,16 @@ namespace AviationWeatherDataProvider
             var context = localPluginContext.PluginExecutionContext;
             var sysService = localPluginContext.SystemUserService;
             var tracer = localPluginContext.TracingService;
-            tracer.Trace("Retrieving METARs...");
 
             QueryExpression query = (QueryExpression)context.InputParameters["Query"];
 
-            (string stationString, string stationIdBeginsWith, string[] antiStationString) =
-                ParseMetarNameQuery(query, tracer);
-            tracer.Trace("Station String: {0}", stationString);
-            tracer.Trace("Station ID Begins With: {0}", stationIdBeginsWith);
-            tracer.Trace("Anti-Station String: {0}", string.Join(",", antiStationString));
+            string stationString = NameConversion.TransformQueryExpression(
+                query,
+                tracer,
+                out var unhandeledNameExpressions
+            );
 
-            EntityCollection entityCollection = new EntityCollection() { EntityName = "awx_metar" };
+            EntityCollection entityCollection;
 
             if (stationString != null)
             {
@@ -45,119 +40,16 @@ namespace AviationWeatherDataProvider
                 entityCollection = GetMetarsByStates(tracer);
             }
 
-            if (antiStationString.Length > 0)
+            if (unhandeledNameExpressions.Any())
             {
-                var filteredEntities = entityCollection
-                    .Entities.Where(e =>
-                        !antiStationString.Contains(e.GetAttributeValue<string>("station_id"))
-                    )
-                    .ToList();
-                entityCollection.Entities.Clear();
-                entityCollection.Entities.AddRange(filteredEntities);
-            }
-            if (stationIdBeginsWith != null)
-            {
-                var filteredEntities = entityCollection
-                    .Entities.Where(e =>
-                        e.GetAttributeValue<string>("station_id").StartsWith(stationIdBeginsWith)
-                    )
-                    .ToList();
-                entityCollection.Entities.Clear();
-                entityCollection.Entities.AddRange(filteredEntities);
+                entityCollection = NameConversion.ProcessUnhandeledExpressions(
+                    entityCollection,
+                    unhandeledNameExpressions,
+                    tracer
+                );
             }
 
             context.OutputParameters["BusinessEntityCollection"] = entityCollection;
-        }
-
-        static (string, string, string[]) ParseMetarNameQuery(
-            QueryExpression query,
-            ITracingService tracer
-        )
-        {
-            //TODO : Implement Like operator
-            string stationString = null;
-            string stationIdBeginsWith = null;
-            string[] antiStationString = { };
-            var nameConditions = query.Criteria.Conditions.Where(q =>
-                q.AttributeName == "awx_name"
-            );
-            tracer.Trace("Name Conditions: {0}", nameConditions.Count());
-            foreach (var condition in nameConditions)
-            {
-                if (stationString != null)
-                {
-                    continue;
-                }
-                switch (condition.Operator)
-                {
-                    case ConditionOperator.Equal:
-                        stationString = condition.Values[0] as string;
-                        break;
-                    case ConditionOperator.NotEqual:
-                        antiStationString = condition.Values.Select(v => v.ToString()).ToArray();
-                        break;
-                    case ConditionOperator.In:
-                        stationString = string.Join(
-                            ",",
-                            condition.Values.Select(v => v.ToString())
-                        );
-                        break;
-                    case ConditionOperator.NotIn:
-                        antiStationString = condition.Values.Select(v => v.ToString()).ToArray();
-                        break;
-                    case ConditionOperator.BeginsWith:
-                        stationIdBeginsWith = condition.Values[0] as string;
-                        break;
-
-                    default:
-                        tracer.Trace("Unsupported operator: {0}", condition.Operator);
-                        break;
-                }
-            }
-            var nameFilters = query.Criteria.Filters.Where(q =>
-                q.Conditions.Any(c => c.AttributeName == "awx_name")
-            );
-            tracer.Trace("Name Filters: {0}", nameFilters.Count());
-            foreach (var filter in nameFilters)
-            {
-                foreach (var condition in filter.Conditions)
-                {
-                    if (stationString != null)
-                    {
-                        continue;
-                    }
-                    switch (condition.Operator)
-                    {
-                        case ConditionOperator.Equal:
-                            stationString = condition.Values[0] as string;
-                            break;
-                        case ConditionOperator.NotEqual:
-                            antiStationString = condition
-                                .Values.Select(v => v.ToString())
-                                .ToArray();
-                            break;
-                        case ConditionOperator.In:
-                            stationString = string.Join(
-                                ",",
-                                condition.Values.Select(v => v.ToString())
-                            );
-                            break;
-                        case ConditionOperator.NotIn:
-                            antiStationString = condition
-                                .Values.Select(v => v.ToString())
-                                .ToArray();
-                            break;
-                        case ConditionOperator.BeginsWith:
-                            stationIdBeginsWith = condition.Values[0] as string;
-                            break;
-                        default:
-                            tracer.Trace("Unsupported operator: {0}", condition.Operator);
-                            break;
-                    }
-                }
-            }
-
-            return (stationString, stationIdBeginsWith, antiStationString);
         }
 
         static EntityCollection GetMetarsByStationString(
@@ -230,6 +122,13 @@ namespace AviationWeatherDataProvider
                         using (StringReader reader = new StringReader(metarAsXml))
                         {
                             var responseObj = (Response)serializer.Deserialize(reader);
+                            if (responseObj.Data.NumResults == 1000)
+                            {
+                                tracer.Trace("Too many results returned. Skipping batch.");
+                                throw new InvalidPluginExecutionException(
+                                    $"Too many results returned for {stationString}"
+                                );
+                            }
                             EntityCollection metars = responseObj.GetResponseMetars(
                                 null,
                                 responseObj
