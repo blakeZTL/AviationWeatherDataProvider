e918fa13b7d0f9d18b8f5d4a52224fbef6b79001
Blake Bradford <b@blrfam.win>
2025-02-23 18:19:20 -0500

    Add Utils.cs with unit conversion methods
    Added Utils.cs to AviationWeatherDataProvider.csproj.
    The new file includes a static Utils class with methods for:
    - Converting temperatures between Fahrenheit and Celsius.
    - Converting pressures between millibars and inches of mercury.
    - Converting distances from meters to feet.

---
+++ AviationWeatherDataProvider/Utils.cs
@@ -0,0 +1,50 @@
+﻿namespace AviationWeatherDataProvider
+{
+    public static class Utils
+    {
+        public static float? ConvertToCelsius(float? fahrenheit)
+        {
+            if (fahrenheit == null)
+            {
+                return null;
+            }
+            return (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static float? ConvertToFahrenheit(float? celsius)
+        {
+            if (celsius == null)
+            {
+                return null;
+            }
+            return celsius * 9 / 5 + 32;
+        }
+
+        public static float? MillibarsToInOfMercury(float? millibars)
+        {
+            if (millibars == null)
+            {
+                return null;
+            }
+            return (float)System.Math.Round((double)(millibars * 0.02953f), 2);
+        }
+
+        public static float? InOfMercuryToMillibars(float? inOfMercury)
+        {
+            if (inOfMercury == null)
+            {
+                return null;
+            }
+            return inOfMercury / 0.02953f;
+        }
+
+        public static int? MetersToFeet(int? meters)
+        {
+            if (meters == null)
+            {
+                return null;
+            }
+            return (int)(meters * 3.28084);
+        }
+    }
+}
