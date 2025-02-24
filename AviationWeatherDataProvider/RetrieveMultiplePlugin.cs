aad53dbcb207978b9c52f6486f29031b070d85f8
Blake Bradford <james.b.bradford@faa.gov>
2025-02-24 11:36:28 -0500

    Refactor filtering logic in RetrieveMultiplePlugin.cs
    Refactored the ExecuteCdsPlugin method to improve filtering logic:
    - Changed unused SystemUserService variable to var _.
    - Introduced filteredEntities to store filtered results.
    - Added logic to handle queries with no filters or conditions.
    - Enhanced filtering for AND/OR logical operators in query criteria.
    - Removed previous unhandled expressions for visibility, weather string, and clouds.
    - Set output parameter BusinessEntityCollection to filteredEntities.
    - Simplified stateBatches assignment in GetMetarsByStates method.

--- AviationWeatherDataProvider/RetrieveMultiplePlugin.cs
+++ AviationWeatherDataProvider/RetrieveMultiplePlugin.cs
@@ -20,7 +20,7 @@ namespace AviationWeatherDataProvider
         protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
         {
             var context = localPluginContext.PluginExecutionContext;
-            var sysService = localPluginContext.SystemUserService;
+            var _ = localPluginContext.SystemUserService;
             var tracer = localPluginContext.TracingService;
 
             QueryExpression query = (QueryExpression)context.InputParameters["Query"];
@@ -38,7 +38,7 @@ namespace AviationWeatherDataProvider
             );
 
             EntityCollection entityCollection;
-
+            var filteredEntities = new EntityCollection();
             try
             {
                 if (stationString != null)
@@ -62,22 +62,215 @@ namespace AviationWeatherDataProvider
                         tracer
                     );
                 }
+
+                if (
+                    !query.Criteria.Filters.Any()
+                    || !query.Criteria.Filters.Any(f => f.Conditions.Count > 0)
+                )
+                {
+                    tracer.Trace("No filters in query");
+                    tracer.Trace("Conditions found: " + query.Criteria.Conditions.Count.ToString());
+                    if (query.Criteria.Conditions.Any())
+                    {
+                        var filter = new FilterExpression
+                        {
+                            FilterOperator = query.Criteria.FilterOperator
+                        };
+                        filter.Conditions.AddRange(query.Criteria.Conditions);
+                        query.Criteria.Filters.Add(filter);
+                    }
+                }
+
+                foreach (var filter in query.Criteria.Filters)
+                {
+                    tracer.Trace($"Filter conditions: {filter.Conditions.Count}");
+                    if (filter.FilterOperator == LogicalOperator.And)
+                    {
+                        var intersectingEntities = new List<Entity>();
+                        var filteredByVis = new EntityCollection();
+                        var filteredByWxString = new EntityCollection();
+                        tracer.Trace("Filter operator is AND");
+                        foreach (var condition in filter.Conditions)
+                        {
+                            tracer.Trace(
+                                "Attribute: {0}, Operator: {1}, Value: {2}",
+                                condition.AttributeName,
+                                condition.Operator,
+                                condition.Values?.Count > 0 ? condition.Values[0] : "null"
+                            );
+                            try
+                            {
+                                var results = VisibilityQueryConversion.GetFilteredCollection(
+                                    entityCollection,
+                                    condition,
+                                    tracer
+                                );
+                                if (filteredByVis.Entities.Count == 0)
+                                {
+                                    tracer.Trace("Visibility count: " + results.Entities.Count);
+                                    filteredByVis.Entities.AddRange(results.Entities);
+                                }
+                                else
+                                {
+                                    tracer.Trace(
+                                        "Visibility count before revision: "
+                                            + results.Entities.Count
+                                    );
+                                    var revisedEntities = results.Entities.Intersect(
+                                        filteredByVis.Entities
+                                    );
+                                    filteredByVis.Entities.Clear();
+                                    filteredByVis.Entities.AddRange(revisedEntities);
+
+                                    tracer.Trace(
+                                        $"Visibility count after revision: {filteredByVis.Entities.Count}"
+                                    );
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                tracer.Trace(ex.ToString());
+                                throw new InvalidPluginExecutionException(
+                                    "Error filtering METARs by visibility."
+                                );
+                            }
+
+                            try
+                            {
+                                var results = WeatherStringQueryConversion.GetFilteredCollection(
+                                    entityCollection,
+                                    condition,
+                                    tracer
+                                );
+                                if (filteredByWxString.Entities.Count == 0)
+                                {
+                                    filteredByWxString.Entities.AddRange(results.Entities);
+                                }
+                                else
+                                {
+                                    var revisedEntities = results.Entities.Where(
+                                        e => filteredByWxString.Entities.Contains(e)
+                                    );
+                                    filteredByWxString.Entities.Clear();
+                                    filteredByWxString.Entities.AddRange(revisedEntities);
+                                }
+                                tracer.Trace(
+                                    $"Weather string count: {filteredByWxString.Entities.Count}"
+                                );
+                            }
+                            catch (Exception ex)
+                            {
+                                tracer.Trace(ex.ToString());
+                                throw new InvalidPluginExecutionException(
+                                    "Error filtering METARs by weather string."
+                                );
+                            }
+                        }
+                        if (filteredByWxString.Entities.Any() && filteredByVis.Entities.Any())
+                        {
+                            tracer.Trace("Both visibility and weather string filters");
+                            intersectingEntities.AddRange(
+                                entityCollection.Entities
+                                    .Intersect(filteredByWxString.Entities)
+                                    .Intersect(filteredByVis.Entities)
+                                    .ToList()
+                            );
+                        }
+                        else if (filteredByWxString.Entities.Any() && !filteredByVis.Entities.Any())
+                        {
+                            tracer.Trace("Only weather string filter");
+                            intersectingEntities.AddRange(
+                                entityCollection.Entities
+                                    .Intersect(filteredByWxString.Entities)
+                                    .ToList()
+                            );
+                        }
+                        else if (!filteredByWxString.Entities.Any() && filteredByVis.Entities.Any())
+                        {
+                            tracer.Trace("Only visibility filter");
+                            intersectingEntities.AddRange(
+                                entityCollection.Entities.Intersect(filteredByVis.Entities).ToList()
+                            );
+                        }
+                        else
+                        {
+                            tracer.Trace("No entities to apply intersection logic with");
+                        }
+                        filteredEntities.Entities.AddRange(intersectingEntities);
+                    }
+                    else if (filter.FilterOperator == LogicalOperator.Or)
+                    {
+                        var unionEntities = new List<Entity>();
+                        foreach (var condition in filter.Conditions)
+                        {
+                            tracer.Trace("OR filter operator");
+                            EntityCollection filteredByVis;
+                            try
+                            {
+                                filteredByVis = VisibilityQueryConversion.GetFilteredCollection(
+                                    entityCollection,
+                                    condition,
+                                    tracer
+                                );
+                                tracer.Trace($"Visibility count: {filteredByVis.Entities.Count}");
+                            }
+                            catch (Exception ex)
+                            {
+                                tracer.Trace(ex.ToString());
+                                throw new InvalidPluginExecutionException(
+                                    "Error filtering METARs by visibility."
+                                );
+                            }
+                            EntityCollection filteredByWxString;
+                            try
+                            {
+                                filteredByWxString =
+                                    WeatherStringQueryConversion.GetFilteredCollection(
+                                        entityCollection,
+                                        condition,
+                                        tracer
+                                    );
+                                tracer.Trace(
+                                    $"Weather string count: {filteredByWxString.Entities.Count}"
+                                );
+                            }
+                            catch (Exception ex)
+                            {
+                                tracer.Trace(ex.ToString());
+                                throw new InvalidPluginExecutionException(
+                                    "Error filtering METARs by weather string."
+                                );
+                            }
+                            try
+                            {
+                                // add filteredVis entities to unionEntities that arent already there
+                                unionEntities.AddRange(
+                                    filteredByVis.Entities
+                                        .Where(entity => !unionEntities.Contains(entity))
+                                        .ToList()
+                                );
+                                // add filteredWxString entities to unionEntities that arent already there
+                                unionEntities.AddRange(
+                                    filteredByWxString.Entities
+                                        .Where(entity => !unionEntities.Contains(entity))
+                                        .ToList()
+                                );
+                                tracer.Trace($"Union count: {unionEntities.Count}");
+                            }
+                            catch (Exception ex)
+                            {
+                                tracer.Trace(ex.ToString());
+                                throw new InvalidPluginExecutionException(
+                                    "Error filtering METARs by visibility or weather string."
+                                );
+                            }
+                        }
+                        filteredEntities.Entities.AddRange(
+                            entityCollection.Entities.Intersect(unionEntities).ToList()
+                        );
+                    }
+                }
                 // TODO: Handle filter logical operators for groupings of conditions with different logical operators
-                entityCollection = VisibilityQueryConversion.ProcessUnhandeledExpressions(
-                    entityCollection,
-                    query,
-                    tracer
-                );
-                entityCollection = WeatherStringQueryConversion.ProcessUnhandeledExpressions(
-                    entityCollection,
-                    query,
-                    tracer
-                );
-                entityCollection = CloudsQueryConversion.ProcessUnhandeledExpressions(
-                    entityCollection,
-                    query,
-                    tracer
-                );
             }
             catch (Exception ex)
             {
@@ -85,7 +278,7 @@ namespace AviationWeatherDataProvider
                 throw new InvalidPluginExecutionException(ex.Message);
             }
 
-            context.OutputParameters["BusinessEntityCollection"] = entityCollection;
+            context.OutputParameters["BusinessEntityCollection"] = filteredEntities;
         }
 
         static EntityCollection GetMetarsByStationString(
@@ -142,8 +335,8 @@ namespace AviationWeatherDataProvider
             EntityCollection entities = new EntityCollection() { EntityName = "awx_metar" };
             using (HttpClient client = new HttpClient())
             {
-                var stateBatches = States
-                    .Initials.Select((state, index) => new { state, index })
+                var stateBatches = States.Initials
+                    .Select((state, index) => new { state, index })
                     .GroupBy(x => x.index / 10)
                     .Select(g => g.Select(x => x.state).ToArray())
                     .ToArray();
