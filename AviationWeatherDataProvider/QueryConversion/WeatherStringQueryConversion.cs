a82d1d41383003c1d58a6916a9865288e3f957e4
Blake Bradford <james.b.bradford@faa.gov>
2025-02-24 11:35:47 -0500

    Refactor WeatherStringQueryConversion and add filtering
    Introduce a static readonly field `attributeName` in the
    `WeatherStringQueryConversion` class to store the attribute name
    "awx_weatherstring". Update `ProcessUnhandeledExpressions` to use
    this field and refactor the method to streamline filtering logic
    and improve readability. Add a new method `GetFilteredCollection`
    to handle filtering of `EntityCollection` based on various
    `ConditionOperator` cases. Include tracing statements for
    debugging and performance monitoring.

--- AviationWeatherDataProvider/QueryConversion/WeatherStringQueryConversion.cs
+++ AviationWeatherDataProvider/QueryConversion/WeatherStringQueryConversion.cs
@@ -9,6 +9,8 @@ namespace AviationWeatherDataProvider.QueryConversion
 {
     public class WeatherStringQueryConversion
     {
+        private static readonly string attributeName = "awx_weatherstring";
+
         public static EntityCollection ProcessUnhandeledExpressions(
             EntityCollection metars,
             QueryExpression query,
@@ -21,14 +23,13 @@ namespace AviationWeatherDataProvider.QueryConversion
                     + nameof(ProcessUnhandeledExpressions)
                     + $" Start: {DateTime.Now}"
             );
-            var attributeName = "awx_weatherstring";
             var conditions = new List<ConditionExpression>();
             conditions.AddRange(
                 query.Criteria.Conditions.Where(c => c.AttributeName == attributeName).ToList()
             );
             conditions.AddRange(
-                query.Criteria.Filters.SelectMany(f =>
-                    f.Conditions.Where(c => c.AttributeName == attributeName)
+                query.Criteria.Filters.SelectMany(
+                    f => f.Conditions.Where(c => c.AttributeName == attributeName)
                 )
             );
 
@@ -43,8 +44,8 @@ namespace AviationWeatherDataProvider.QueryConversion
                     case ConditionOperator.Like:
                         var likeValue = exp.Values[0] as string;
                         var regexPattern = "^" + Regex.Escape(likeValue).Replace("%", ".*") + "$";
-                        var likeEntities = metars
-                            .Entities.Where(m =>
+                        var likeEntities = metars.Entities
+                            .Where(m =>
                             {
                                 var entityValue = m.GetAttributeValue<string>(attributeName);
                                 var isMatch = Regex.IsMatch(entityValue, regexPattern);
@@ -61,8 +62,8 @@ namespace AviationWeatherDataProvider.QueryConversion
                         var notLikeValue = exp.Values[0] as string;
                         var notLikePattern =
                             "^" + Regex.Escape(notLikeValue).Replace("%", ".*") + "$";
-                        var notLikeEntities = metars
-                            .Entities.Where(m =>
+                        var notLikeEntities = metars.Entities
+                            .Where(m =>
                             {
                                 var entityValue = m.GetAttributeValue<string>(attributeName);
                                 var isMatch = !Regex.IsMatch(entityValue, notLikePattern);
@@ -77,8 +78,8 @@ namespace AviationWeatherDataProvider.QueryConversion
                         break;
 
                     case ConditionOperator.Null:
-                        var nullEntities = metars
-                            .Entities.Where(m =>
+                        var nullEntities = metars.Entities
+                            .Where(m =>
                             {
                                 var isNull =
                                     m.Attributes.ContainsKey(attributeName)
@@ -93,8 +94,8 @@ namespace AviationWeatherDataProvider.QueryConversion
                         metars.Entities.AddRange(nullEntities);
                         break;
                     case ConditionOperator.NotNull:
-                        var notNullEntities = metars
-                            .Entities.Where(m =>
+                        var notNullEntities = metars.Entities
+                            .Where(m =>
                             {
                                 var isNotNull =
                                     m.Attributes.ContainsKey(attributeName)
@@ -124,6 +125,104 @@ namespace AviationWeatherDataProvider.QueryConversion
                     + $" End: {DateTime.Now}"
             );
             return metars;
+        }
+
+        public static EntityCollection GetFilteredCollection(
+            EntityCollection metars,
+            ConditionExpression exp,
+            ITracingService tracer
+        )
+        {
+            tracer.Trace(
+                nameof(WeatherStringQueryConversion)
+                    + "."
+                    + nameof(GetFilteredCollection)
+                    + $" Start: {DateTime.Now}"
+            );
+
+            var filteredMetars = new EntityCollection();
+            if (exp.AttributeName != attributeName)
+            {
+                tracer.Trace(
+                    $"Attribute name {exp.AttributeName} does not match the expected attribute name {attributeName}"
+                );
+                return filteredMetars;
+            }
+
+            switch (exp.Operator)
+            {
+                case ConditionOperator.Like:
+                    var likeValue = exp.Values?[0] as string;
+                    var regexPattern = "^" + Regex.Escape(likeValue).Replace("%", ".*") + "$";
+                    var likeEntities = metars.Entities
+                        .Where(m =>
+                        {
+                            var entityValue = m.GetAttributeValue<string>(attributeName);
+                            var isMatch = Regex.IsMatch(entityValue, regexPattern);
+                            //tracer.Trace(
+                            //    $"Comparing for Like: Entity Value = {entityValue}, Pattern = {regexPattern}, IsMatch = {isMatch}"
+                            //);
+                            return isMatch;
+                        })
+                        .ToList();
+                    filteredMetars.Entities.AddRange(likeEntities);
+                    break;
+                case ConditionOperator.NotLike:
+                    var notLikeValue = exp.Values?[0] as string;
+                    var notLikePattern = "^" + Regex.Escape(notLikeValue).Replace("%", ".*") + "$";
+                    var notLikeEntities = metars.Entities
+                        .Where(m =>
+                        {
+                            var entityValue = m.GetAttributeValue<string>(attributeName);
+                            var isMatch = !Regex.IsMatch(entityValue, notLikePattern);
+                            //tracer.Trace(
+                            //    $"Comparing for NotLike: Entity Value = {entityValue}, Pattern = {notLikePattern}, IsMatch = {isMatch}"
+                            //);
+                            return isMatch;
+                        })
+                        .ToList();
+                    filteredMetars.Entities.AddRange(notLikeEntities);
+                    break;
+
+                case ConditionOperator.Null:
+                    var nullEntities = metars.Entities
+                        .Where(m =>
+                        {
+                            var isNull =
+                                m.Attributes.ContainsKey(attributeName) && m[attributeName] == null;
+                            //tracer.Trace(
+                            //    $"Comparing for Null: Entity Value = {m.GetAttributeValue<string>(attributeName)}, IsNull = {isNull}"
+                            //);
+                            return isNull;
+                        })
+                        .ToList();
+                    filteredMetars.Entities.AddRange(nullEntities);
+                    break;
+                case ConditionOperator.NotNull:
+                    var notNullEntities = metars.Entities
+                        .Where(m =>
+                        {
+                            var isNotNull =
+                                m.Attributes.ContainsKey(attributeName) && m[attributeName] != null;
+                            //tracer.Trace(
+                            //    $"Comparing for NotNull: Entity Value = {m.GetAttributeValue<string>(attributeName)}, IsNotNull = {isNotNull}"
+                            //);
+                            return isNotNull;
+                        })
+                        .ToList();
+                    filteredMetars.Entities.AddRange(notNullEntities);
+                    break;
+                default:
+                    tracer.Trace($"Unhandeled expression: {exp.Operator}");
+                    break;
+            }
+            tracer.Trace(
+                nameof(WeatherStringQueryConversion)
+                    + "."
+                    + nameof(GetFilteredCollection)
+                    + $" End: {DateTime.Now}"
+            );
+            return filteredMetars;
         }
     }
 }
